92b47d63ddb878b649d1cc4d4cfdc7b8fbe70bdb
blueswen <blueswenlove@gamil.com>
2017-04-06 16:33:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd040603/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.yvtc.yvd040603;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    InputStream inputStream;
+    ByteArrayOutputStream os;
+    byte[] buffer;
+    ImageView img;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        os = new ByteArrayOutputStream();
+        img = (ImageView) findViewById(R.id.imageView);
+        buffer = new byte[64];
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    URL url = new URL("https://uc.udn.com.tw/photo/2017/04/06/1/3364635.jpg");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    int total = conn.getContentLength();
+                    int current = 0;
+                    inputStream = conn.getInputStream();
+                    int readSize = 0;
+                    while((readSize = inputStream.read(buffer)) != -1)
+                    {
+                        os.write(buffer, 0, readSize);
+                        current += readSize;
+                        final int c = current;
+                        final int t = total;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv.setText(String.valueOf(100 * c / t));
+                            }
+                        });
+
+                    }
+                    byte[] result = os.toByteArray();
+                    final Bitmap bitmap = BitmapFactory.decodeByteArray(result, 0, result.length);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            img.setImageBitmap(bitmap);
+                        }
+                    });
+
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+    }
+}
